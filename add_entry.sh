dc77a26ef136f2c21172f7f282a06e1dfb91b842
Priscilla Rega <priscilla.rega@gmail.com>
2019-09-04 10:49:50 -0300

    Update add_entry.sh

--- add_entry.sh
+++ add_entry.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Adiciona a entrada HOSTNAME e IP no servidor BIND9. $1 aguarda o hostanme e $2 aguarda o IP. Sera preenchido pelo formulario do vRO.
+# $1 é o hostname, $2 é o endereço IP, $3 é o servidor DNS e $4 é a zona DNS.
 
-echo "server dns1.hl.local" > /tmp/nsupdate
+echo "server $3" > /tmp/nsupdate
 echo "debug yes" >> /tmp/nsupdate
-echo "zone hl.local." >> /tmp/nsupdate
+echo "zone $4." >> /tmp/nsupdate
 echo "update add $1 600 A $2" >> /tmp/nsupdate
 echo "send" >> /tmp/nsupdate
 
